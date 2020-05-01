8aab049884a71f304135d053c85dd95a84763716
m4-z3 <melissajzhang@gmail.com>
2020-05-01 17:16:57 -0500

    minor error checking

--- text2bullets.py
+++ text2bullets.py
@@ -1,18 +1,23 @@
 from lda import TopicModeling
 from sent_class import classify
 import sys
+import os
 
 def main():
+    # initialize lda stuff
     lda_modeling = TopicModeling()
+    # get file contents
     with open(sys.argv[1], 'r', encoding='utf-8') as txt:
         paragraphs = txt.readlines()
     
     output = []
 
+    # go through all paragraphs
     for paragraph in paragraphs:
         groupedSentence = lda_modeling.groupSentence(paragraph) 
         output += classify(groupedSentence)
 
+    # write to output file
     with open('Notes.txt', 'w') as file:
         file.writelines(output)
 
@@ -21,5 +26,7 @@ if __name__ == "__main__":
         sys.exit('Please pass in a txt file')
     elif len(sys.argv) > 2:
         sys.exit('Too many arguments passed in')
-
-    main()
+    if os.path.isfile(sys.argv[1]):
+        main()
+    else:
+        sys.exit("File doesn't exist")
